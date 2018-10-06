cf4908416e6c8ab2bc6413012ed8ebe136f37c9d
Ariel Noyman <noyman@mit.edu>
2018-10-06 17:09:44 -0400

    LLDD comments

--- GSVscraper/LatLonDisDir.py
+++ GSVscraper/LatLonDisDir.py
@@ -1,3 +1,34 @@
+# # # # # # # # # # # # # # # #
+
+# {{ DLFLV }}
+# Copyright (C) {{ 2017 }}  {{ Ariel Noyman }}
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# # # # # # # # # # # # # # # # #
+
+# "@context": "https://github.com/RELNO/", "@type": "Person", "address": {
+# "@type": "75 Amherst St, Cambridge, MA 02139", "addressLocality":
+# "Cambridge", "addressRegion": "MA",},
+# "jobTitle": "Research Scientist", "name": "Ariel Noyman",
+# "alumniOf": "MIT", "url": "http://arielnoyman.com",
+# "https://www.linkedin.com/", "http://twitter.com/relno",
+# https://github.com/RELNO]
+# # # # # # # # # # # # # # # #
+#
+# USAGE:
+# python3 GSVScraper.py -l __FILE NAME__ -i __ANGLE__ -k __GOOGLEKEY__
+#
+# # # # # # # # # # # # # # # #
+
 '''
 ----------------------------
 LLDD[LatLonDisDir] finds GSV
