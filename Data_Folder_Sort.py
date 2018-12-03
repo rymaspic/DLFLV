19bf65c116bd0ea98cb74a7517791dc6f076cdeb
rymaspic <rymaspic@outlook.com>
2018-12-03 14:41:21 -0500

    fixed a bug

--- Data_Folder_Sort.py
+++ Data_Folder_Sort.py
@@ -55,21 +55,21 @@ for image in cluster_image_list:
         train_counter = train_counter + 1
         train_cluster_des = "DL_Model/adrNet1.0/image_data/data_12.3/train/clusters/" + str(train_counter) + '.jpg'
         shutil.copy(image_path, train_cluster_des)
-        print(train_cluster_des)
+        #print(train_cluster_des)
 
     elif counter >= (1 + train) and counter <= (train + val):
         val_counter = val_counter + 1
         val_cluster_des = "DL_Model/adrNet1.0/image_data/data_12.3/val/clusters/" + str(val_counter) + '.jpg'
         shutil.copy(image_path, val_cluster_des)
-        print(val_cluster_des)
+        #print(val_cluster_des)
         #shutil.copy(image_path, des)
     elif counter >= (1 + train + val) and counter <= (train + val + test):
         test_counter = test_counter + 1
         test_cluster_des = "DL_Model/adrNet1.0/image_data/data_12.3/test/clusters/" + str(test_counter) + '.jpg'
         shutil.copy(image_path, test_cluster_des)
-        print(test_cluster_des)
+        #print(test_cluster_des)
         #shutil.copy(image_path, des)
-    print(test_counter)
+    #print(test_counter)
 
 
 counter = 0
@@ -79,23 +79,23 @@ test_counter = 0
 
 for image in non_cluster_image_list:
     #print(image)
-    nc_image_path = os.path.join(cluster_source_folder, image)
+    nc_image_path = os.path.join(non_cluster_source_folder, image)
     counter = counter + 1
     if counter >= 1 and counter <= train:
         train_counter = train_counter + 1
         train_non_cluster_des = "DL_Model/adrNet1.0/image_data/data_12.3/train/no_clusters/" + str(train_counter) + '.jpg'
-        shutil.copy(image_path, train_non_cluster_des)
-        print(train_non_cluster_des)
+        shutil.copy(nc_image_path, train_non_cluster_des)
+        #print(train_non_cluster_des)
 
     elif counter >= (1 + train) and counter <= (train + val):
         val_counter = val_counter + 1
         val_non_cluster_des = "DL_Model/adrNet1.0/image_data/data_12.3/val/no_clusters/" + str(val_counter) + '.jpg'
-        print(val_non_cluster_des)
-        shutil.copy(image_path, val_non_cluster_des)
+        #print(val_non_cluster_des)
+        shutil.copy(nc_image_path, val_non_cluster_des)
     elif counter >= (1 + train + val) and counter <= (train + val + test):
         test_counter = test_counter + 1
         test_non_cluster_des = "DL_Model/adrNet1.0/image_data/data_12.3/test/no_clusters/" + str(test_counter) + '.jpg'
-        print(test_non_cluster_des)
+        #print(test_non_cluster_des)
         #print(test_counter)
-        shutil.copy(image_path, test_non_cluster_des)
+        shutil.copy(nc_image_path, test_non_cluster_des)
     #print(test_counter)
