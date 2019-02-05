4423df6b25f1be5980e6d4a0fbc26e3bbbe69ab1
rymaspic <rymaspic@outlook.com>
2019-02-05 16:07:04 -0500

    add argparse to simplify usage

--- GSVscraper/locations_gmplot.py
+++ GSVscraper/locations_gmplot.py
@@ -1,10 +1,29 @@
 import json
 from pprint import pprint
 import gmplot
+import argparse
 
-cluster_locations_path = "adr_cluster_locations.json"
-api_key = ""
-map_html = "adr_cluster_map.html"
+# # # # # # # # # # # # # # # #
+#
+# USAGE:
+# python3 -k your_google_key -locations path_to_location_JSON_file -map name_of_map_html
+#
+# # # # # # # # # # # # # # # #
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+
+ap.add_argument("-k", "--key", required=True,
+                help="Google Places key")
+ap.add_argument("-filename", "--locations", required=True,
+                help="File path of the locations")
+ap.add_argument("-mapname", "--map", required=True,
+                help="Html name of the map to be saved")
+args = vars(ap.parse_args())
+
+cluster_locations_path = args["locations"]
+# cluster_locations_path = "adr_cluster_locations.json"
+api_key = args["key"]
+map_html = args["map"]
 #read the saved JSON documents which contain the locations
 with open(cluster_locations_path,'r') as f:
     data = json.load(f)
